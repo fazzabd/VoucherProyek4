db267a630a7753b8514fb5b25d93f64c267106ec
fazzabd <faizabdussalam@gmail.com>
2018-05-07 17:22:13 +0700

    tambah Module

---
+++ Voucher and Coupon/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Voucher and Coupon',
+    'summary': "Odoo POS Vouchers",
+    'description': "Now enhance your sale using our module “Odoo POS Vouchers”. This module allows you to create and redeem vouchers/coupons from Point of Sale. 
+	Using “Odoo POS Vouchers”, you can provide special offers to the customers and also it plays an important role in reactivating your old customers because they are already aware of your services and would be attracted if you provide them special discounts/offers. 
+	A POS user creates vouchers for the customers in which specific code is mentioned. This code is used to provide a flat or percent based discount, on customers order."
+
+    'depends': ['sale', 'account'],
+    'data': [],
+    'images': ['static/description/icon.png'],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+}
