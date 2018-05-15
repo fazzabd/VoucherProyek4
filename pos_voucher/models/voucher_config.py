35f5e86a08a216b4ce5bfa34e3ebb16cb7d10387
Kartika Sari <kartika.sari.tif16@polban.ac.id>
2018-05-15 10:36:43 +0700

    benerin config
    default dan required

--- pos_voucher/models/voucher_config.py
+++ pos_voucher/models/voucher_config.py
@@ -7,17 +7,22 @@ class VoucherConfigPos(models.Model) :
 	_name = 'voucher.config'
 	_description = 'Voucher Configuration'
 
-	active =  fields.Boolean('Active')
+	active =  fields.Boolean('Active',default=True)
 	customer_type = fields.Selection([
         ('specific', 'Specific Customer'),
         ('all', 'All Customer'),
         ], string='Customer Type', required=True)
+	voucher_usage = fields.Selection([
+        ('posEcommerce', 'Both POS & Ecommerce'),
+		('eco', 'Ecommerce'),
+        ('pos', 'Point of Sales'),
+        ], string='Coupon Used In',default='posEcommerce', required=True)
 	default_availability = fields.Integer('Total Available',required=True)
-	default_name = fields.Char('Name',required=True)
-	default_validity = fields.Integer('Validity(in days)', required=True)
+	default_name = fields.Char('Name')
+	default_validity = fields.Integer('Validity(in days)')
 	default_value = fields.Float('Voucher Value', required=True)
 	max_amount = fields.Float('Maximum Voucher Value', required=True)
-	max_expiry_date = fields.Date('Maximum Expiry Date', required=True)
+	max_expiry_date = fields.Date('Maximum Expiry Date', default=datetime.now(),required=True)
 	min_amount = fields.Float('Minimum Voucher Value', required=True)
 	minimum_cart_amount = fields.Float('Minimum Cart Amount')
 	name = fields.Char('Name')
