49525e9f330bedbb6a0e24c224c81837d8e9b927
Kartika Sari <kartika.sari.tif16@polban.ac.id>
2018-05-21 23:35:48 +0700

    test
    coba

--- pos_voucher/models/__init__.py
+++ pos_voucher/models/__init__.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
 from . import voucher
 from . import voucher_config
-=======
-from . import voucher , voucher_config , voucher_history
->>>>>>> 548bb91bbb067db1be8429564a394aaf0a402f46
+from . import voucher_history
--- pos_voucher/models/voucher.py
+++ pos_voucher/models/voucher.py
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
 # -*- coding : utf-8 -*-
-=======
-# -*- coding : utf-8 -*- 
->>>>>>> 548bb91bbb067db1be8429564a394aaf0a402f46
 from odoo import models, fields, api
 from datetime import datetime
 from datetime import timedelta
 
-import random
-import string
-
 class VoucherPOS(models.Model) :
-<<<<<<< HEAD
     _name = 'voucher'
     _inherit=['mail.thread']
     _description = 'Voucher POS'
@@ -84,46 +76,3 @@ class VoucherPOS(models.Model) :
             'target': 'new',
             'context': ctx,
         }
-=======
-	_name = 'voucher'
-	_inherit=['mail.thread']
-	_description = 'Voucher POS'
-	
-	name = fields.Char('Name',required=True)
-	voucher_code = fields.Char('Code')
-	voucher_usage = fields.Selection([
-        ('posEcommerce', 'Both POS & Ecommerce'),
-		('eco', 'Ecommerce'),
-        ('pos', 'Point of Sales'),
-        ], string='Coupon Used In',default='posEcommerce', required=True)
-	customer_type = fields.Selection([
-        ('specific', 'Specific Customer'),
-        ('all', 'All Customer'),
-        ], string='Coupon For', default='specific', required=True)
-	customer_id = fields.Many2one('res.partner','Created For', required=True)
-	active = fields.Boolean('Active',default=True)
-	validity = fields.Integer('Validity(In days)')
-	expiry_date = fields.Date('Expiry Date')
-	issue_date = fields.Date('Applicable From',default=datetime.now(),required=True)
-	voucher_val_type = fields.Selection([
-        ('persen', '%'),
-        ('fix', 'Fixed'),
-        ], string='Voucher val type')
-	voucher_value = fields.Float('Voucher Value',required=True)
-	minimum_cart_amount = fields.Float('Minimum Cart Amount',required=True)
-	use_minimum_cart_value = fields.Boolean('Use Cart Amount Validation')
-	is_partially_redemed = fields.Boolean('Use Partial Redemption')
-	redeemption_limit = fields.Integer('Max Redemption Limit',required=True)
-	applied_on = fields.Selection([
-		('all', 'All Products'),
-        ('specific', 'Specific Product'),
-        ], string='Voucher Applied On',required=True)
-	product_ids = fields.Many2many('product.template')
-	display_desc_in_web = fields.Boolean('Display Description in Website',default=True)
-	note = fields.Text('Description')
-	
-
-	# @api.model
-	# def _get_default_code():
- #    	return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
->>>>>>> 548bb91bbb067db1be8429564a394aaf0a402f46
